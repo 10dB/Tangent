fa662cf912a5e516c96158ed9c3e58a41a6c2156
sc8696 <evenstevens280@gmail.com>
2012-01-14 17:29:28 +0000

    AAAH

---
+++ Tangent/src/com/jonathanfinerty/tangent/TangentActivity.java
@@ -0,0 +1,16 @@
+package com.jonathanfinerty.tangent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TangentActivity extends Activity {
+   /** Called when the activity is first created. */
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       TextView tv = new TextView(this);
+       tv.setText("FUCCCCCCCCCCK");
+       setContentView(tv);
+   }
+}
